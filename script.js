04ed8b782ccbae1fd7d5f282b478c4e83c128047
yassine263 <yassine.aydir@etu.uae.ac.ma>
2025-05-08 19:55:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-console.log("======== verifier le type avec typeof =======");
-console.log(typeof 42);
-console.log(typeof "hello");
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof {});
-console.log(typeof []);
-console.log(typeof function(){});
-
-
-console.log("======== verifier le type avec instanceof ( verfie si objet est une instance type) =======");
-console.log([] instanceof Array);
-console.log({} instanceof Object);
-console.log(new Date() instanceof Date);
-console.log(function (){} instanceof Function );
-
-
-console.log("======== verifier le type avec array.isarray() ( verfie si est un tableau) =======");
-console.log(Array.isArray([]));
-console.log(Array.isArray({}));
-console.log(Array.isArray("hello"));
-
-
-console.log("======== verifier le type avec Number.isInteger() ( verfie si est un nombre entier) =======");
-console.log(Number.isInteger(10));
-console.log(Number.isInteger(10.5));
-console.log(Number.isInteger("10"));
 document.getElementById("registrationForm").addEventListener("submit", function(e) {
     e.preventDefault();
   
